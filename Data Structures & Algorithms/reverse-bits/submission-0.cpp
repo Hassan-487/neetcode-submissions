c347058fedddc2e5f71d43f67d583d658c6f39e1
Hassan-487 <139991237+Hassan-487@users.noreply.github.com>
2026-05-07 03:24:38 +0500

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int r=0;
+
+        for(int i=0;i<32;i++){
+            r=r<<1;
+            r=r|(n&1);
+            n=n>>1;
+        }
+        return r;
+    }
+};
