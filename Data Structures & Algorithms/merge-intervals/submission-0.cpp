4e716311c814cda3c49d13c9415a8f8d4c35e249
Hassan-487 <139991237+Hassan-487@users.noreply.github.com>
2026-05-08 00:38:29 +0500

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        if (intervals.empty()) return res;
+        sort(intervals.begin(), intervals.end());
+
+        for (int i = 0; i < intervals.size(); i++) {
+            if (res.empty() || res.back()[1] < intervals[i][0]) {
+                res.push_back(intervals[i]);
+            } 
+
+            else {
+               
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+
+        return res;
+    }
+};
