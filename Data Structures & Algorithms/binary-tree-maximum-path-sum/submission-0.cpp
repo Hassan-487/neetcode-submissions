90c0bbb1df7af40520af532b8391d6dc7a1fa590
Hassan-487 <139991237+Hassan-487@users.noreply.github.com>
2026-06-14 22:24:00 +0500

    Add: binary-tree-maximum-path-sum - submission-0

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int dfs(TreeNode* root, int& ans) {
+        if(root == nullptr)
+            return 0;
+
+        int left = max(0, dfs(root->left, ans));
+        int right = max(0, dfs(root->right, ans));
+
+        ans = max(ans, left + right + root->val);
+
+        return root->val + max(left, right);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+
+        dfs(root, ans);
+
+        return ans;
+    }
+};
